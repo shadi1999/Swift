91d25ceb212efada9d75096efd38e252e762410c
Ubuntu <ubuntu@ip-172-31-5-166.me-south-1.compute.internal>
2020-04-19 13:54:54 +0000

    fixed slides

---
+++ public/precache-manifest.789beeab9944dc93a51387e6380620b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a89c077ee7583877c9f3de02f57960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d401378fda3ecb6b372a",
+    "url": "/static/css/2.0491e0c4.chunk.css"
+  },
+  {
+    "revision": "93c309fb8420fa441e3f",
+    "url": "/static/css/main.26d30aa3.chunk.css"
+  },
+  {
+    "revision": "d401378fda3ecb6b372a",
+    "url": "/static/js/2.459d3398.chunk.js"
+  },
+  {
+    "revision": "76b6dc1e1b42332a2323569236f9d595",
+    "url": "/static/js/2.459d3398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93c309fb8420fa441e3f",
+    "url": "/static/js/main.b3f844dc.chunk.js"
+  },
+  {
+    "revision": "7d85c22fed949e89fd38",
+    "url": "/static/js/runtime-main.bde67853.js"
+  },
+  {
+    "revision": "3a32a11f6ed46446e0e3f1fcb19fd19e",
+    "url": "/static/media/default-monochrome.3a32a11f.svg"
+  }
+]);
